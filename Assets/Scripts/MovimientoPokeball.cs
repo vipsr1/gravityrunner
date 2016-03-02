2c4bba659c0d92f35fc746965c7da8bd3810acd4
vipsr1 <davidruso6@gmail.com>
2016-03-02 12:35:39 +0100

    Primer commit

---
+++ Assets/Scripts/MovimientoPokeball.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovimientoPokeball : MonoBehaviour {
+
+    public float moveSpeed = 5f;
+    public float rotacion = 10f;
+    float range;
+    
+
+    // Use this for initialization
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            
+        }
+    }
+
+    
+}
